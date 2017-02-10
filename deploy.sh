6e1c99c43b543ca62b914f738404204f3ebcfa30
Gar Morley <garmorley@gmail.com>
2017-02-10 09:25:39 +0000

    Changes to deploy script and add instructions to README
    We were copying the assets to the wrong place in the Rails app, so the
    stylesheets etc. were not being picked up when we deployed. The last
    line in the deploy script fixes that now.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 bundle exec middleman build --clean
-cp -r build/* ~/projects/cognito/public/docs/api/
+cp -r build/* ~/projects/cognito/public/docs/
+mv ~/projects/cognito/public/docs/index.html ~/projects/cognito/public/docs/api/
+
